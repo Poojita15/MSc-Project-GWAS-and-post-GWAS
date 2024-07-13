b7ccb55c6839276de6b7ab2197f5a5d1eb4a6846
Poojita Karchalkar <149092844+Poojita15@users.noreply.github.com>
2024-07-13 16:13:04 +0100

    Create multipletesting.R

---
+++ Genetic correlations/multipletesting.R
@@ -0,0 +1,12 @@
+#########################################
+Multiple testing correction 
+#########################################
+
+# 
+
+scp PD.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/pd
+scp PDM.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/pd
+scp PDF.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/pd
+scp DE.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/de
+scp DEF.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/de
+scp DEM.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/de
