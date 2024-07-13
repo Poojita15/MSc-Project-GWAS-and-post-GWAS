a66eac97ca28a6dc3b185aeaff72e08baf5fe196
Poojita Karchalkar <149092844+Poojita15@users.noreply.github.com>
2024-07-13 16:14:50 +0100

    Update multipletesting.R

--- Genetic correlations/multipletesting.R
+++ Genetic correlations/multipletesting.R
@@ -2,7 +2,7 @@
 Multiple testing correction 
 #########################################
 
-# 
+# Secure copy the result files from local machine (Windows path) to remote server
 
 scp PD.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/pd
 scp PDM.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/pd
@@ -10,3 +10,5 @@ scp PDF.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgv
 scp DE.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/de
 scp DEF.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/de
 scp DEM.csv bt23718@login.hpc.qmul.ac.uk:/data/scratch/bt23718/GWAS_project/ctgvl/de
+
+
