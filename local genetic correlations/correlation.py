af8d670be3db04d9d5881a46d408d579d8072f89
Poojita Karchalkar <149092844+Poojita15@users.noreply.github.com>
2024-07-27 12:12:26 +0100

    Update correlation.py

--- local genetic correlations/correlation.py
+++ local genetic correlations/correlation.py
@@ -13,7 +13,7 @@ python3 supergnova.py data/sumstats/pd/pd.sumstats.gz data/sumstats/de/de.sumsta
 --N2 460961 \
 --bfile data/bfiles/eur_chr@_SNPmaf5 \
 --partition data/partition/eur_chr@.bed \
---out all.tx
+--out all.txt
 
 # calulate local genetic correlations with papulosqamous disorders and dermatitis (male samples)
 
